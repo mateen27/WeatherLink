dab219d07aabca3794192593cf32f1737f7651fd
mateen27 <khanmateen2713@gmail.com>
2023-08-21 14:36:45 +0530

    done with the UI of the application

---
+++ App.js
@@ -0,0 +1,29 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/Home';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown : false}}>
+        <Stack.Screen name='Home' component={Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+    <StatusBar style='light'/>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
